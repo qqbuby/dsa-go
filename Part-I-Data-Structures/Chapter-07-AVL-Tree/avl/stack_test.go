3b56d92f5ab878a0516378b06ed273b92b43a973
qqbuby <qqbuby@gmail.com>
2020-05-05 21:59:40 +0800

    Fix format error

--- Part-I-Data-Structures/Chapter-07-AVL-Tree/avl/stack_test.go
+++ Part-I-Data-Structures/Chapter-07-AVL-Tree/avl/stack_test.go
@@ -11,15 +11,15 @@ func TestStack(t *testing.T) {
 	s.Push(&Node{Value: 15})
 
 	if v, _ := s.Pop(); v.Value != 15 {
-		t.Errorf("s.Pop() should be %d, got: %d", 15, v)
+		t.Errorf("s.Pop() should be %d, got: %d", 15, v.Value)
 	}
 
 	if v, _ := s.Peek(); v.Value != 11 {
-		t.Errorf("s.Peek() should be %d, got: %d", 11, v)
+		t.Errorf("s.Peek() should be %d, got: %d", 11, v.Value)
 	}
 
 	if v, _ := s.Pop(); v.Value != 11 {
-		t.Errorf("s.Pop() should be %d, got: %d", 11, v)
+		t.Errorf("s.Pop() should be %d, got: %d", 11, v.Value)
 	}
 
 	if _, ok := s.Pop(); ok {
